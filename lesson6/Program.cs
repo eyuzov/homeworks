a4eb3b01da53cfb7310258621f0a474cb7230125
eyuzov <eyuzov@gmail.com>
2022-07-13 09:20:14 +0300

    Add lesson6 task2

--- lesson6/Program.cs
+++ lesson6/Program.cs
@@ -36,3 +36,29 @@
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+// Так как,по условию значения (Y) функций совпадают,то:
+// k1 * x + b1 = k2 * x + b2
+// k1 * x - k2 * x = b2 - b1
+// x * (k1 - k2) = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+x = Math.Round(x, 2);
+y = Math.Round(y, 2);
+
+if (k1 * x + b1 == k2 * x + b2) Console.WriteLine($"Пересечение прямых в точке: ({x};{y})");
+
+else Console.WriteLine("Эти прямые паралельны или совпадают");
