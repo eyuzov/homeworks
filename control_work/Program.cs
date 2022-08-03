c3412606c8a0a712da79e44ab69961301f00d4f1
eyuzov <eyuzov@gmail.com>
2022-08-03 21:44:56 +0300

    Add C# code for task

---
+++ control_work/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+// string[] array = { "1234", "1567", "-2", "computer science" };
+// string[] array = { "Russia", "Denmark", "Kazan" };
+
+int NewArrayLenght(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] resultArray = new string[NewArrayLenght(array)];
+
+
+for (int i = 0; i < resultArray.Length; i++)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j].Length <= 3)
+        {
+            resultArray[i] = array[j];
+            i++;
+        }
+    }
+}
+
+
+
+
+
+System.Console.WriteLine(string.Join(", ", resultArray));
