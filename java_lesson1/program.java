f7307b14132465dda773af4e34f4f6d7058b4e80
eyuzov <eyuzov@gmail.com>
2022-12-06 22:59:38 +0300

    Added tasks

---
+++ java_lesson1/program.java
@@ -0,0 +1,57 @@
+package java_projects.homeworks.java_lesson1;
+
+public class program {
+    public static void main(String[] args) {
+        // ========================= 1-я задача==================================
+
+        // ========================= 2-я задача==================================
+        // int x, y;
+        // for (x = 1; x < 1000; x++) {
+        // y = 0;
+        // for (int i = 1; i <= x; i++) {
+        // if (x % i == 0)
+        // y++;
+        // }
+        // if (y <= 2)
+        // System.out.println(x);
+
+        // }
+
+        // ========================= 3-я задача==================================
+
+        // Scanner iScanner = new Scanner(System.in);
+
+        // System.out.printf("input first number: ");
+        // int firstNumber = iScanner.nextInt();
+
+        // System.out.printf("input second number: ");
+        // int secondNumber = iScanner.nextInt();
+
+        // System.out.printf("Input operation: ");
+        // String str = iScanner.next();
+
+        // double res = 0;
+        // switch (str) {
+        // case "*":
+        // res = firstNumber * secondNumber;
+        // System.out.println("Result: " + res);
+        // break;
+        // case "/":
+        // res = firstNumber / secondNumber;
+        // System.out.println("Result: " + res);
+        // break;
+        // case "+":
+        // res = firstNumber + secondNumber;
+        // System.out.println("Result: " + res);
+        // break;
+        // case "-":
+        // res = firstNumber - secondNumber;
+        // System.out.println("Result: " + res);
+        // break;
+        // default:
+        // break;
+        // }
+        // iScanner.close();
+
+    }
+}
