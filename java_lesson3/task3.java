3322d4f438d67e32b27d3903c9b587d95ea23654
eyuzov <eyuzov@gmail.com>
2022-12-13 17:08:38 +0300

    Added task3

--- java_lesson3/task3.java
+++ java_lesson3/task3.java
@@ -10,6 +10,36 @@ public class task3 {
     public static void main(String[] args) {
         // Задан целочисленный список ArrayList. Найти минимальное, максимальное и
         // среднее из этого списка.
+
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        arr.add(11);
+        arr.add(45);
+        arr.add(12);
+        arr.add(32);
+        arr.add(37);
+        arr.add(47);
+
+        int max = arr.get(0);
+        int min = arr.get(0);
+        double avg = 0;
+        int res = 0;
+
+        for (int item : arr) {
+            if (item > max) {
+                max = item;
+            }
+            if (item < min) {
+                min = item;
+            }
+            res = res + item;
+        }
+
+        avg = res / arr.size();
+
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(avg);
     }
 
 }
