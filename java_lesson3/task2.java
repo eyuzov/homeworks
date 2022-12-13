989bb4e688eca034d791bb2fb95a87ea01d89a9f
eyuzov <eyuzov@gmail.com>
2022-12-13 16:17:36 +0300

    Fix some warnings

--- java_lesson3/task2.java
+++ java_lesson3/task2.java
@@ -1,15 +1,13 @@
 package java_projects.homeworks.java_lesson3;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class task2 {
 
     public static void main(String[] args) {
         // Пусть дан произвольный список целых чисел, удалить из него четные числа
 
-        List<Integer> arr = new ArrayList<Integer>();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
 
         arr.add(11);
         arr.add(45);
@@ -17,7 +15,7 @@ public class task2 {
         arr.add(32);
         arr.add(37);
 
-        List<Integer> newArr = new ArrayList<Integer>();
+        ArrayList<Integer> newArr = new ArrayList<Integer>();
 
         System.out.println(arr.toString());
 
--- java_lesson3/task3.java
+++ java_lesson3/task3.java
@@ -8,7 +8,8 @@ import java.util.Random;
 public class task3 {
 
     public static void main(String[] args) {
-
+        // Задан целочисленный список ArrayList. Найти минимальное, максимальное и
+        // среднее из этого списка.
     }
 
 }
