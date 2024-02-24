01a10588a8c84e5af04444ddee48b0504f6a7820
eyuzov <eyuzov@gmail.com>
2024-02-24 09:38:15 +0300

    change publicPath

--- VueJS/lesson4/vue.config.js
+++ VueJS/lesson4/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/lesson4/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/homeworks/" : "/",
   pages: {
     index: {
       entry: "src/main.js",
